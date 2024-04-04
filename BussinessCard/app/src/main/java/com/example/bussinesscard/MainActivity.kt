a1285f02fecc3cc4a9e7029668a34ab66a70eb06
Danilo A. Santos <danilo_as1992@hotmail.com>
2024-04-03 22:50:48 -0300

    Update

--- BussinessCard/app/src/main/java/com/example/bussinesscard/MainActivity.kt
+++ BussinessCard/app/src/main/java/com/example/bussinesscard/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.bussinesscard
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bussinesscard.ui.theme.BussinessCardTheme
 
@@ -40,50 +49,78 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainCard(modifier: Modifier = Modifier) {
 
-    Column(modifier = modifier) {
-        ImageCard(modifier)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.background(color = Color(0XFFD2E8D4))
+    ) {
+        ImageCard(modifier.align(alignment = Alignment.CenterHorizontally).padding(top = 120.dp))
         MainText(modifier.align(alignment = Alignment.CenterHorizontally))
-        MainContact(modifier)
+        MainContact(modifier/*.align(alignment = Alignment.Start)*/)
     }
 }
 
 @Composable
 fun ImageCard (modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.android_logo)
-
     Image(
         painter = image,
         contentDescription = null,
-        contentScale = ContentScale.Inside
+        contentScale = ContentScale.Crop,
+        modifier = modifier.size(80.dp).background(color = Color(0XFF073042))
     )
 }
 
 @Composable
 fun MainText (modifier: Modifier = Modifier) {
     Text(
-        text = "Full name",
-        fontSize = 36.sp,
-        modifier = modifier
+        text = "Danilo Alves dos Santos",
+        fontSize = 22.sp,
+        color = Color.Black,
+        modifier = modifier.padding(start = 15.dp, end = 15.dp)
     )
     Text(
-        text = "Title",
-        fontSize = 24.sp,
-        modifier = modifier
+        text = "Android Developer",
+        fontSize = 16.sp,
+        color = Color(0,107,54),
+        modifier = modifier.padding(bottom = 36.dp, top = 10.dp)
     )
 }
 
 @Composable
 fun MainContact(modifier: Modifier = Modifier) {
-    Column (modifier = modifier) {
-        Contact(modifier)
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.padding(top = 20.dp)
+    ) {
+        val imagePhone = painterResource(id = R.drawable.icon_phone)
+        Contact(imagePhone, "+55 (14) 98166-6513        ", modifier)
+        val imageShare = painterResource(id = R.drawable.icon_share)
+        Contact(imageShare,"@DaniloAlves                     ",modifier)
+        val imageMail = painterResource(id = R.drawable.icon_mail)
+        Contact(imageMail, "daniloas1992@gmail.com",modifier)
     }
 }
 
 @Composable
-fun Contact(image: Image, contact: String, modifier: Modifier = Modifier) {
-    Row (modifier = modifier) {
+fun Contact(image: Painter, contact: String, modifier: Modifier = Modifier) {
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 15.dp),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier.size(25.dp)
+        )
+        Spacer(Modifier.width(12.dp))
         Text(
-            text = "+00 (00) 000-000"
+            text = contact,
+            color = Color.Black,
+            modifier = modifier
         )
     }
 }
